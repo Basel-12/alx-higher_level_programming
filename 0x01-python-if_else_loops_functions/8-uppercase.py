39d7f84cca82c8cf60059cf0bd7ddb546ca4c5ce
Basel-12 <bassilhamdi410@gmail.com>
2024-06-12 19:33:49 +0300

    alx push

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    s = ""
     for c in  str:
         if (c >= 'a' and c <='z'):
             c = chr(ord(c) - 32)
-        s += c
-        print("{}".format(s))
+        print("{}".format(c), end="")
+        print("")
