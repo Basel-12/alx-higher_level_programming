96efbe8f9dcf63e6890cee9534237c3d48883da1
Basel-12 <bassilhamdi410@gmail.com>
2024-06-12 21:07:25 +0300

    alx push

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -17,8 +17,15 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *cur = *head, *temp;
 	listint_t *new = (listint_t *)malloc(sizeof(listint_t));
 
-	if (!new || !*head)
+	if (!new || !head)
 		return (NULL);
+	if (!*head)
+	{
+		new->n = number;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	while (cur)
 	{
 		temp = cur->next;
