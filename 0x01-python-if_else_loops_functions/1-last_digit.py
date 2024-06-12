d81b6b2d15a097c35d98e55c47ba38eed23c0896
Basel-12 <bassilhamdi410@gmail.com>
2024-06-12 11:20:09 +0300

    alx push

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-ldigit = number % 10;
+ldigit = abs(number)%10;
 print(f"Last digit of {number} is {ldigit} ",end = "")
 if ldigit > 5:
     print("and is greater than 5")
