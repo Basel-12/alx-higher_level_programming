a9ea79f60b17855f0f2f52e1cfa28844bc56c51e
Basel-12 <bassilhamdi410@gmail.com>
2024-06-16 19:19:31 +0300

    alx push

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list == None:
+        return
     size = len(my_list) - 1
     for i in range(size, -1, -1):
         print("{:d}".format(my_list[i]))
