3012fb49c896bfda6fb02ce157b11580fdae33fd
Basel-12 <bassilhamdi410@gmail.com>
2024-06-19 19:26:42 +0300

    alx push

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const n = parseInt(process.argv[2]);
 if (isNaN(n)) { console.log('Missing size'); } else if (n > 0) {
-  const mystr = 'x'.repeat(n);
+  const mystr = 'X'.repeat(n);
   for (let i = 0; i < n; i++) { console.log(mystr); }
 }
