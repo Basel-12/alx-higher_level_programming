ba955ac83bd945cb25f425ce5193d451cb6b2d16
Basel-12 <bassilhamdi410@gmail.com>
2024-06-19 19:35:51 +0300

    alx push

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function factorial (a) {
+  if (a === 0 || a === 1)
+	return 1;
+  return a * factorial(a - 1);
+}
+const n1 = parseInt(process.argv[2]);
+if (isNaN(n1)) { console.log(1); } else {
+  console.log(factorial(n1));
+}
