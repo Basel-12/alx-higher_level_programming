0f6673e0cabe9f933c8024d01c93b8923a648fcf
Basel-12 <bassilhamdi410@gmail.com>
2024-06-21 11:44:06 +0300

    alx push

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    set = {}
+    myset = set() 
     sum = 0;
     for i in my_list:
-        set.add(i)
-    for i in set:
+        myset.add(i)
+    for i in myset:
         sum += i
     return sum
