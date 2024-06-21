ea87c19e7efd5ce868734744856b36f30eb92f3e
Basel-12 <bassilhamdi410@gmail.com>
2024-06-21 12:47:42 +0300

    alx push

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     maxvalue = 0
     maxkey = None
+    if a_dictionary === None:
+        return None
     for key, value in a_dictionary.items():
         if value > maxvalue:
             maxvalue = value
